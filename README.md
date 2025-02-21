9fd00b19033d55a1d72a160f565b70a4c6621337
dwgan <dwgan@foxmail.com>
2025-02-21 17:43:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Intent-Control
+
+这是一个用于意图识别的项目，利用与训练的[bert-slot](https://huggingface.co/google-bert/bert-base-chinese/tree/main)进行微调，添加了智能领域的数据进行微调，可用于控制空调、灯、门的开关。
+
+配合语音识别模块，可实现智能语音控制。
+
+# 参考
+
+> https://github.com/Linear95/bert-intent-slot-detector
+> https://github.com/mzc421/NLP/tree/main/bert-intent-slot
---
+++ predict.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+# @author: 木子川
+# @Email:  m21z50c71@163.com
+# @VX：fylaicai
+
+
+from detector import JointIntentSlotDetector
+import time
+
+start1_time = time.perf_counter()
+model = JointIntentSlotDetector.from_pretrained(
+    model_path='./result/model/model/model_epoch6',
+    tokenizer_path='./result/model/model/model_epoch6',
+    intent_label_path='./data/SMP2019/intent_labels.txt',
+    slot_label_path='./data/SMP2019/slot_labels.txt'
+)
+start2_time = time.perf_counter()
+all_text = ['请打开卧室的灯', "开门", "关上门", "给老王发短信说我饿了", "打开厨房的灯", "打开厨房空调"]
+for i in all_text:
+    print(model.detect(i))
+end_time = time.perf_counter()
+time1 = (end_time - start1_time) / 3600
+time2 = (end_time - start2_time) / 3600
+print("所有检测时间（包括加载模型）：", time1, "s", "除去模型加载时间：", time2, "s",
+      "总预测数据量：", len(all_text), "平均预测一条的时间（除去加载模型）：", time2 / len(all_text), "s/条")
